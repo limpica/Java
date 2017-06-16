f1e54078e13d48e53070e03a21e426f7d4dfcfeb
Mengyin Bian <609279037@qq.com>
2017-06-16 21:24:33 +0800

    Update AccountSerialize.java

--- AccountSerialize.java
+++ AccountSerialize.java
@@ -23,9 +23,9 @@ public class AccountSerialize {
 		readAccountFromTxt("固定科目.txt");
 		serialize("Accounts.ser");
 		deserialize("Accounts.ser");
-		readAccountFromTxt("动态科目.txt");
-		serialize("Accounts.ser");
-		deserialize("Accounts.ser");
+		//readAccountFromTxt("动态科目.txt");
+		//serialize("Accounts.ser");
+		//deserialize("Accounts.ser");
 	}
 
 	// 读数据到arraylist accounts
@@ -38,13 +38,26 @@ public class AccountSerialize {
 			String line; // 循环存储读入的一行的内容
 			
 			accounts.clear(); // 每次读入前先把之前的清空一遍
-
+			//int i = 0;
+			
 			while ((line = br.readLine()) != null) {
 				String acc_number = line.split(" ")[0]; // 科目代码（acc_number）和科目名称（acc_name）是用空格分开的
 				String acc_name = new String(line.split(" ")[1]);
+				String acc_entry;
+				String acc_def;	
+				
+				if(line.split(" ").length >= 3)//只找了少部分科目的借贷方向，所以需要判断一下
+					acc_entry = new String(line.split(" ")[2]);
+				else acc_entry = null;
+					
+				if (line.split(" ").length >= 4) //只找了少部分科目的定义，所以需要判断一下
+					acc_def = new String(line.split(" ")[3]);
+				else acc_def = null;
 				//不确定读入结果是不是正确，可以打印一下
-				//System.out.println(acc_number+ " " + acc_name);
-				accounts.add(new Account(acc_number, acc_name));
+				//System.out.println(acc_number + "|" + acc_name +"|" + acc_entry + "|" + acc_def);
+				accounts.add(new Account(acc_number, acc_name, acc_entry, acc_def));
+				//System.out.println(accounts.get(i).getAcc_number()+ "|" + accounts.get(i).getAcc_name() + "|" + accounts.get(i).getAcc_entry() + accounts.get(i).getAcc_def());
+				//i = i + 1;
 			}
 			br.close();
 
@@ -52,6 +65,7 @@ public class AccountSerialize {
 			System.out.println("1: " + ex);
 		}
 	}
+
 
 	public static void serialize(String filename) {
 		try {
@@ -80,7 +94,7 @@ public class AccountSerialize {
 			for (int m = 0; m < size; m++) {
 				accounts.add((Account) input.readObject());
 				//不确定反序列化是不是正确，可以打印一下
-				//System.out.println(((Account)accounts.get(m)).getAcc_number() + ((Account)accounts.get(m)).getAcc_name() );
+				//System.out.println(((Account)accounts.get(m)).getAcc_number() + ((Account)accounts.get(m)).getAcc_name()+ ((Account)accounts.get(m)).getAcc_def() );
 			}
 			input.close();
 
